04504a8ded33177249043665cac02e8688a85c21
Matthew Stopa <matthew.p.stopa@gmail.com>
2012-05-19 12:48:13 -0400

    Add more tests

--- spec/simpleview_spec.rb
+++ spec/simpleview_spec.rb
@@ -2,11 +2,33 @@ require './lib/simpleview.rb'
 
 describe "SimpleView" do
   describe "#parse" do
-    context "with basic inputs" do
+    context "with a single line" do
       let(:template) { "Hi, {name}!" }
       let(:hash) { { "name" => "John" } }
       subject { Simpleview.new.parse(hash, template) }
       it { should == "Hi, John!\n" }
+    end
+
+    context "with two lines" do
+      let(:template) { "Hi, {name}!\nWhat do you like about {city}?" }
+      let(:hash) { { "name" => "John", "city" => "Atlanta" } }
+      subject { Simpleview.new.parse(hash, template) }
+      it { should == "Hi, John!\nWhat do you like about Atlanta?\n" }
+    end
+
+    context "multiple lines with nested fields" do
+      let(:template) { "Hi, {name}!\n{#family}\nHow is your {relationship}:\n{name}\n{/family}" }
+      let(:hash)  {
+                    {
+                      "name"=>"John",
+                      "family"=>[
+                         { "name" => "Mr. Moocow", "relationship" => "father"},
+                         { "name" => "Piglet", "relationship" => "daughter"}
+                      ]
+                    }
+                  }
+      subject { Simpleview.new.parse(hash, template) }
+      it { should == "Hi, John!\nHow is your father:\nMr. Moocow\nHow is your daughter:\nPiglet\n" }
     end
   end
 end
