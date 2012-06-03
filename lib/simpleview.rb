bcc9d252f8962820ff885c80ae639e8bb30a8347
Matthew Stopa <matthew.p.stopa@gmail.com>
2012-06-03 01:33:15 -0400

    Add multiple tags on lines and refactor

--- lib/simpleview.rb
+++ lib/simpleview.rb
@@ -6,35 +6,72 @@ class Simpleview
     lines_to_skip = 0
     hash_scope = hash
     lines = template.class == Array ? template : template.split("\n")
+
     lines.each_with_index do |line, index|
       if lines_to_skip > 0
         lines_to_skip -= 1
         next
       end
-      start = line =~ /{/
-      stop  = line[start..line.length] =~ /}/ if start != nil
-      if start.nil? || stop.nil?
-        result += "#{line}\n"
-        next
-      end
-      token = line[start..stop+start]
-      token_value = token[1..token.length-2]
-      if token_value[0] == "#"
-        varscope = hash_scope[token_value.gsub("#", '')]
-        snippet = generate_snippet(lines, index)
-        varscope.each do |s|
-          r = parse(s, snippet)
-          result += r if r.class == String
+
+      if scope_key = starts_sub_context?(tokenize(line))
+        scope_key = strip_to_context_key(scope_key)
+        sub_context_lines = grab_subcontext_lines(lines, index)
+        sub_scope = hash_scope[scope_key]
+        sub_scope.each do |scope|
+          sub_context_lines.each do |l|
+            result += process_line(l, scope)
+          end
         end
-        lines_to_skip = snippet.length + 1
+        lines_to_skip = sub_context_lines.size + 1
         next
       end
-      result += line.sub(token, hash_scope[token_value]) + "\n"
+      result += process_line(line, hash_scope)
     end
     result
   end
 
-  def generate_snippet(lines, index)
+  def process_line(line, scope)
+    tokens = tokenize(line)
+    tokens = replace_tokens_with_values(tokens, scope)
+    tokens.inject() { |old, n| "#{old}#{n}" } + "\n"
+  end
+
+  def strip_to_context_key(token)
+    token[2..token.size-2]
+  end
+
+  def starts_sub_context?(tokens)
+    tokens.detect { |t| t[0..1] == '{#' }
+  end
+
+  def ends_sub_context?(tokens)
+    tokens.detect { |t| t[0..1] == '{/' }
+  end
+
+  def replace_tokens_with_values(tokens, hash_scope)
+    tokens.each_with_index do |token, index|
+      if token[0] == '{'
+        token_value = token[1..token.length-2]
+        tokens[index] = hash_scope[token_value]
+      end
+    end
+    tokens
+  end
+
+  def tokenize(line)
+    results = []
+    [].tap do |arr|
+      chomped_line = line
+      loop do
+        results = chomped_line.partition(/{(.*?)}/).reject { |s| s == "" }
+        break if results.size == 1
+        arr.push(results - [results.last])
+        chomped_line = results.last
+      end
+    end.push(results).flatten
+  end
+
+  def grab_subcontext_lines(lines, index)
     snippet = lines[index+1..lines.length]
     end_location = snippet.find_index { |l| l =~ /{\// }
     lines[index+1, end_location]
--- spec/simpleview_spec.rb
+++ spec/simpleview_spec.rb
@@ -9,11 +9,25 @@ describe "SimpleView" do
       it { should == "Hi, John!\n" }
     end
 
+    context "with a single line with just a token" do
+      let(:template) { "{name}" }
+      let(:hash) { { "name" => "John" } }
+      subject { Simpleview.new.parse(hash, template) }
+      it { should == "John\n" }
+    end
+
     context "with two lines" do
       let(:template) { "Hi, {name}!\nWhat do you like about {city}?" }
       let(:hash) { { "name" => "John", "city" => "Atlanta" } }
       subject { Simpleview.new.parse(hash, template) }
       it { should == "Hi, John!\nWhat do you like about Atlanta?\n" }
+    end
+
+    context "with a single line having multiple tags on it" do
+      let(:template) { "Hi, {name}! My name is {your_name}." }
+      let(:hash) { { "name" => "John", "your_name" => "Matt" } }
+      subject { Simpleview.new.parse(hash, template) }
+      it { should == "Hi, John! My name is Matt.\n" }
     end
 
     context "multiple lines with nested fields" do
@@ -51,9 +65,187 @@ describe "SimpleView" do
     end
   end
 
-  describe "#generate_snippet" do
+  describe "#process_line" do
+    context "with no tags" do
+      let(:tokens) { "name" }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.process_line(tokens, scope)}
+      it { should == "name\n" }
+    end
+
+    context "with only a tag and nothing else" do
+      let(:tokens) { "{name}" }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.process_line(tokens, scope)}
+      it { should == "John\n" }
+    end
+
+    context "with a single tag" do
+      let(:tokens) { "Have a great day {name}. Life is grande." }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.process_line(tokens, scope)}
+      it { should == "Have a great day John. Life is grande.\n" }
+    end
+
+    context "with interspersed tags" do
+      let(:tokens) { "{greeting} Have a great day {name}. Life is grande. {ending}" }
+      let(:scope) { { "greeting" => "Hello!", "name" => "John", "ending" => "See ya later" } }
+      subject { Simpleview.new.process_line(tokens, scope)}
+      it { should == "Hello! Have a great day John. Life is grande. See ya later\n" }
+    end
+  end
+
+  describe "#strip_to_context_key" do
+    let(:token) { "{#first_key}"}
+    subject { Simpleview.new.strip_to_context_key(token) }
+    it { should == "first_key" }
+  end
+
+  describe "#starts_sub_context?" do
+    context "when it does start a sub context" do
+      subject { Simpleview.new.starts_sub_context?(tokens) }
+
+      context "when it is the last token" do
+        let(:tokens) { ["this", "{#"] }
+        it { should_not == nil }
+      end
+
+      context "when it is the first token" do
+        let(:tokens) { ["{#", "this"] }
+        it { should_not == nil }
+      end
+
+      context "when it is in the middle of the token stream" do
+        let(:tokens) { ["blah", "another_token", "{#", "222", "this"] }
+        it { should_not == nil }
+      end
+    end
+
+    context "when it does not start a sub context" do
+      subject { Simpleview.new.starts_sub_context?(tokens) }
+
+      context "when there are no matching tokens" do
+        let(:tokens) { ["blah", "another_token", "#--{", "222", "this"] }
+        it { should == nil }
+      end
+    end
+  end
+
+  describe "#ends_sub_context?" do
+    context "when it does end a sub context" do
+      subject { Simpleview.new.ends_sub_context?(tokens) }
+
+      context "when it is the last token" do
+        let(:tokens) { ["this", "{/"] }
+        it { should_not == nil }
+      end
+
+      context "when it is the first token" do
+        let(:tokens) { ["{/", "this"] }
+        it { should_not == nil }
+      end
+
+      context "when it is in the middle of the token stream" do
+        let(:tokens) { ["blah", "another_token", "{/", "222", "this"] }
+        it { should_not == nil }
+      end
+    end
+
+    context "when it does not end a sub context" do
+      subject { Simpleview.new.ends_sub_context?(tokens) }
+
+      context "when there are no matching tokens" do
+        let(:tokens) { ["blah", "another_token", "{#", "222", "this"] }
+        it { should == nil }
+      end
+    end
+  end
+
+  describe "#replace_tokens_with_values" do
+    context "with no tags" do
+      let(:tokens) { ["name"] }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.replace_tokens_with_values(tokens, scope)}
+      it { should == ["name"] }
+    end
+
+    context "with only a tag and nothing else" do
+      let(:tokens) { ["{name}"] }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.replace_tokens_with_values(tokens, scope)}
+      it { should == ["John"] }
+    end
+
+    context "with a single tag" do
+      let(:tokens) { ["Have a great day", "{name}", ". Life is grande."] }
+      let(:scope) { {"name" => "John" } }
+      subject { Simpleview.new.replace_tokens_with_values(tokens, scope)}
+      it { should == ["Have a great day", "John", ". Life is grande."] }
+    end
+
+    context "with interspersed tags" do
+      let(:tokens) { ["{greeting}", "Have a great day", "{name}", ". Life is grande.", "{ending}"] }
+      let(:scope) { { "greeting" => "Hello!", "name" => "John", "ending" => "See ya later" } }
+      subject { Simpleview.new.replace_tokens_with_values(tokens, scope)}
+      it { should == ["Hello!", "Have a great day", "John", ". Life is grande.", "See ya later"] }
+    end
+  end
+
+  describe "#tokenize" do
+    context "line without a tag" do
+      subject { Simpleview.new.tokenize("Hello, what a nice day")}
+      it { should == ["Hello, what a nice day"] }
+    end
+
+    context "with a single tag" do
+      context "with only a tag" do
+        subject { Simpleview.new.tokenize("{tag}")}
+        it { should == ["{tag}"] }
+      end
+
+      context "with a tag at the beginning of a line" do
+        subject { Simpleview.new.tokenize("{tag} more text here")}
+        it { should == ["{tag}", " more text here"] }
+      end
+
+      context "with a tag at the end of a line" do
+        subject { Simpleview.new.tokenize("more text here {tag}")}
+        it { should == ["more text here ", "{tag}"] }
+      end
+
+      context "with a tag in the middle of a line" do
+        subject { Simpleview.new.tokenize("more text here {tag} more text after")}
+        it { should == ["more text here ", "{tag}", " more text after"] }
+      end
+    end
+
+    context "with multiple tags on a line" do
+      context "with only a tags" do
+        subject { Simpleview.new.tokenize("{tag}{other_tag}") }
+        it { should == ["{tag}", "{other_tag}"] }
+      end
+
+      context "with multiple tags at the beginning of a line" do
+        subject { Simpleview.new.tokenize("{tag}{other_tag} more text here")}
+        it { should == ["{tag}", "{other_tag}", " more text here"] }
+      end
+
+      context "with multiple tags at the end of a line" do
+        subject { Simpleview.new.tokenize("more text here {tag}{other_tag}")}
+        it { should == ["more text here ", "{tag}","{other_tag}"] }
+      end
+
+      context "with multiple tags in the middle of a line" do
+        subject { Simpleview.new.tokenize("more text here {tag}{other_tag} more text after")}
+        it { should == ["more text here ", "{tag}", "{other_tag}", " more text after"] }
+      end
+    end
+
+  end
+
+  describe "#grab_subcontext_lines" do
     let(:lines) { ["aaa", "bbb", "ccc", "ddd", "eee", "{/}fff", "ggg"]}
-    subject { Simpleview.new.generate_snippet(lines, 1) }
+    subject { Simpleview.new.grab_subcontext_lines(lines, 1) }
     it { should == ["ccc", "ddd", "eee"] }
   end
 end
